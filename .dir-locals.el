4f7592b3ccb03cc2625bcc1e9b3469b3b8ac01aa
Shawn Xu <xu107288696@gmail.com>
2023-10-31 21:17:30 -0700

    Added .clang-format and .dir-locals.el

---
+++ .dir-locals.el
@@ -0,0 +1,6 @@
+((c++-mode . ((flycheck-clang-include-path . ("/opt/homebrew/Cellar/boost/1.83.0/include"))
+              (flycheck-clang-args . ("-I" "/opt/homebrew/Cellar/boost/1.83.0/include" "-lboost_program_options"))
+	      (c-file-style . "bsd")
+              (c-basic-offset . 4)
+              (tab-width . 4)
+              (indent-tabs-mode . nil))))
