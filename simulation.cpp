4d8f974b38480da5c35b10505da01ccad5902bb9
Shawn Xu <xu107288696@gmail.com>
2023-11-01 12:34:47 -0700

    Support multiple threads

--- simulation.cpp
+++ simulation.cpp
@@ -1,20 +1,27 @@
 #include <boost/program_options.hpp>
 
 #include <cstdint>
+#include <future>
+#include <iomanip>
 #include <iostream>
+#include <numeric>
 #include <random>
+#include <thread>
 
 namespace po = boost::program_options;
 
-double simulate(int64_t value);
-bool simulate_once(std::mt19937 &generator, std::discrete_distribution<> &twodice);
+int64_t simulate(int64_t value);
+bool simulate_once(std::mt19937 &generator,
+                   std::discrete_distribution<> &twodice);
 
 int main(int argc, char **argv)
 {
     po::options_description description("Usage");
     description.add_options()("help", "prints this message")(
         "size", po::value<int64_t>()->default_value(1000),
-        "the size of the simulation");
+        "the size of the simulation")("concurrency",
+                                      po::value<int32_t>()->default_value(1),
+                                      "number of core(s) to use");
 
     po::positional_options_description pdescription;
     pdescription.add("size", 1);
@@ -36,18 +43,34 @@ int main(int argc, char **argv)
     if (vm.count("size"))
     {
         int64_t value = vm["size"].as<int64_t>();
+        int32_t thread_num = vm["concurrency"].as<int32_t>();
         std::cout << "Running " << value << " simulations...\n";
-        std::cout << simulate(value) << std::endl;
+        std::vector<std::future<int64_t>> future_results;
+        future_results.reserve(thread_num);
+        for (int i = 0; i < thread_num - 1; i++)
+        {
+            future_results.push_back(std::async(simulate, value / thread_num));
+        }
+        future_results.push_back(
+            std::async(simulate, value / thread_num + value % thread_num));
+
+        std::cout << std::fixed << std::setprecision(15)
+                  << static_cast<long double>(std::accumulate(
+                         future_results.begin(), future_results.end(), 0,
+                         [](int64_t partial_sum, std::future<int64_t> &x)
+                         { return partial_sum + x.get(); })) /
+                         value
+                  << std::endl;
     }
+
     return 0;
 }
 
-double simulate(int64_t size)
+int64_t simulate(int64_t size)
 {
     std::random_device rd;
     std::mt19937 generator(rd());
-    std::discrete_distribution<int> twodice(
-        {1, 2, 3, 4, 5, 6, 5, 4, 3, 2, 1});
+    std::discrete_distribution<int> twodice({1, 2, 3, 4, 5, 6, 5, 4, 3, 2, 1});
 
     int64_t total = 0;
     for (int64_t i = 0; i < size; i++)
@@ -61,10 +84,11 @@ double simulate(int64_t size)
             total--;
         }
     }
-    return static_cast<double>(total) / size;
+    return total;
 }
 
-bool simulate_once(std::mt19937 &generator, std::discrete_distribution<> &twodice)
+bool simulate_once(std::mt19937 &generator,
+                   std::discrete_distribution<> &twodice)
 {
     int initial_roll = twodice(generator);
     int second_roll;
